53d6c2bff083770d2fdb02480774f0ec395bc85d
Tonybanks90 <antonygitonga90@gmail.com>
2025-11-05 13:29:43 +0300

    sanity intergration

---
+++ studio-simply-homed/schemaTypes/index.ts
@@ -0,0 +1,11 @@
+import { categoryType } from './categoryType';
+import { blogPostType } from './blogPostType';
+import { productType } from './productType';
+import { siteSettingsType } from './siteSettingsType';
+
+export const schemaTypes = [
+  categoryType,
+  blogPostType,
+  productType,
+  siteSettingsType,
+];
